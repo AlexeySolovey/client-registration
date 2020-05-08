284a4cd5e01ec3cce12f7324b961ee24ff519dab
alexei <soloveyalexey3@gmail.com>
2020-05-08 11:09:14 +0300

    added server

--- server.js
+++ server.js
@@ -1,52 +1,19 @@
 //Install express server
 const express = require('express');
-const bodyParser = require("body-parser");
+// const path = require('path'); !!!!!!!!!!11
 const cors = require("cors");
-const path = require('path');
 
-const PORT = process.env.PORT || 4444;
-
-const api = require("./server/routes/api");
 const app = express();
 
-app.use(cors());
-app.use(bodyParser.json());
+// app.use(cors()); !!!!!!!!!!11
+
+// Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/angular-registration'));
-app.use("/api", api);
 
 app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+'/dist/angular-registration/index.html'));
-    // const events = [
-    //     {
-    //       _id: "1",
-    //       name: "event1",
-    //       description: "lorem1 lorem1 lorem1",
-    //       data: "2020-03-17"
-    //     },
-    //     {
-    //       _id: "3",
-    //       name: "event3",
-    //       description: "lorem3 lorem3 lorem3",
-    //       data: "2020-03-17"
-    //     },
-    //     {
-    //       _id: "5",
-    //       name: "event1",
-    //       description: "lorem5 lorem5 lorem5",
-    //       data: "2020-03-17"
-    //     },
-    //     {
-    //       _id: "7",
-    //       name: "event7",
-    //       description: "lorem7 lorem7 lorem7",
-    //       data: "2020-03-17"
-    //     }
-    //   ];
-    //   res.json(events);
+    
+res.sendFile(path.join(__dirname+'/dist/angular-registration/index.html'));
 });
 
-
-
-app.listen(PORT, function() {
-   console.log("Server running on localhost:" + PORT);
-});
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
